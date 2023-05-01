6aa1574febca8580ceb26239268f1d00cc7ad78b
Rohyoh <119382502+Rohyoh@users.noreply.github.com>
2023-04-30 23:24:36 -0600

    si

--- Scripts/Js/script.js
+++ Scripts/Js/script.js
@@ -1,9 +1,14 @@
 let searchForm = document.querySelector('.search-form');
+const giBtn = document.getElementById("gi-btn");
+const peppaBtn = document.getElementById("peppa-btn");
 
-document.querySelector('#search-btn').onclick = () =>{
+document.querySelector('#search-btn').onclick = () => {
     searchForm.classList.toggle('active');
 }
 
-window.onscroll = () =>{
+window.onscroll = () => {
     searchForm.classList.remove('active');
 }
+
+giBtn.addEventListener("click", () => alert("Genshin Impact lore"));
+peppaBtn.addEventListener("click", () => window.location.href = "https://www.youtube.com/watch?v=GBIIQ0kP15E");
